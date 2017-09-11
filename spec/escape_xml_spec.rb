bc4f41c72914d6034d23d8c99bf87d51df1ae0aa
Andrew Ageev <ageev86@gmail.com>
2017-09-11 23:14:27 +1000

    Try to fix CI

--- spec/escape_xml_spec.rb
+++ spec/escape_xml_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe String do
   describe '#escape_xml' do
     subject { string.escape_xml }
 
-    let(:param) { [[/&/, '&amp;'], [/</, '&lt;'], [/>/, '&gt;']] }
-
     it do
       should eq '&lt;w:t&gt;&amp;&lt;/w:t&gt;'
     end
@@ -19,14 +17,14 @@ RSpec.describe String do
 
     context 'string has new lines' do
       let(:string) do
-        <<~STRING.strip
+        <<~HEREDOC
           One Line
           Two Line
           Three Line
-        STRING
+        HEREDOC
       end
 
-      it { should eq 'One Line<w:br/>Two Line<w:br/>Three Line' }
+      it { should eq 'One Line<w:br/>Two Line<w:br/>Three Line<w:br/>' }
     end
 
     context 'string has no new lines' do
