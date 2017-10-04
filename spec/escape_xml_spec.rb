740fc48ac676f150d4226b832f197dd080ddb8e7
Andrew Ageev <ageev86@gmail.com>
2017-10-04 18:40:01 +1000

    Add refact test

--- spec/escape_xml_spec.rb
+++ spec/escape_xml_spec.rb
@@ -12,6 +12,32 @@ RSpec.describe String do
     end
   end
 
+  describe '#refact' do
+    subject { string.refact }
+
+    let(:expression) do
+      '<w:t>{</w:t><w:r><w:r><w:t>%= </w:t><w:t>some expressions</w:t><w:t> %}</w:t>'
+    end
+    let(:string) { expression }
+    let(:result) { '<w:t>{%= some expressions %}</w:t>' }
+
+    it { should eq result }
+
+    context 'with brakets' do
+      let(:string) { '<w:t>{{%= </w:t><w:t>some expressions </w:t><w:t>%}</w:t><w:t>}</w:t>' }
+      let(:result) { '<w:t>{{%= some expressions %}</w:t><w:t>}</w:t>' }
+
+      it { should eq result }
+    end
+
+    context 'when image is nearby' do
+      let(:picture) { '<a:ext uri="{28A0092B-C50C-407E-A947-70E740481C1C}"><other tags>' }
+      let(:string) { [picture, expression].join }
+
+      it { should eq [picture, result].join }
+    end
+  end
+
   describe '#convert_newlines' do
     subject { string.convert_newlines }
 
